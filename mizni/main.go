80bca9229c4b2444fbedeb0218dcab244f6c1302
Hong Chen <hong.chen@futurewei.com>
2019-09-23 09:23:53 -0700

    minor: + todo remainder

--- mizni/main.go
+++ mizni/main.go
@@ -56,6 +56,7 @@ func cmdDel(args *skel.CmdArgs) error {
 	}
 	defer netns.Close()
 
+	// todo: stuff a concrete object of unplugger
 	var unplugger unplugger
 	return detachVNICs(unplugger, vnics.NICs)
 }
